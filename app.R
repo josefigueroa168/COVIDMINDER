fc2d4a90341332ffe24000383d99c4c58716a51f
TiburonB <tiburonbenavides@gmail.com>
2020-08-10 14:10:19 -0400

    Shiny App Improvements

--- app.R
+++ app.R
@@ -18,12 +18,12 @@ ui <- fluidPage(
       sliderInput(inputId = "bins",
                   h3("Number of Days to Predict"),
                   min = 1,
-                  max = 100,
+                  max = 10,
                   value = 5),
       
       selectInput("select1", h3("Variable to Predict"), 
-                  choices = list("cases", "deaths", 
-                                 "population","p_cases", 
+                  choices = list("cases", "deaths",
+                                 "p_cases", 
                                  "p_deaths", "diff", 
                                  "p_diff", "d_diff", 
                                  "p.d_diff"), selected = "cases"),
@@ -40,15 +40,26 @@ ui <- fluidPage(
                                  "SC", "SD", "TN", "TX", "UT", 
                                  "VT", "VA", "WA", "WV", "WI", 
                                  "WY"
-                  ), selected = "AL")
+                  ), selected = "AL"),
+    
+      # Input: Slider for top % counties to show ----
+      sliderInput(inputId = "filter_c",
+                  h3("What percent of counties to show"),
+                  min = 0,
+                  max = 100,
+                  value = 100)
+      
     ),
+    
+    
     
     # Main panel for displaying outputs ----
     mainPanel(
       
       plotOutput("ts_plot", height = "1000px"),
       
-      plotOutput("state_plot", height = "1000px")
+      tableOutput("current_counties")
+      #plotOutput("state_plot", height = "1000px")
       
     )
   )
@@ -58,22 +69,27 @@ ui <- fluidPage(
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-get_frames <- function () {
-  
-}
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
   
   output$ts_plot <- renderPlot({
     
     library(forecast)
     library(dplyr)
-    library("plyr")
     library(tidyverse)
     
     n_days = input$bins
     state = input$select2
     col = input$select1
+    filter_c = input$filter_c
+    
+    
+    if (filter_c == 0) {
+      ts.plot()
+    }
+    
+    
     covid_TS_counties_long.pred <- read_csv("data/csv/time_series/covid_TS_counties_long.pred.csv")
 
     data <- covid_TS_counties_long.pred
@@ -131,18 +147,71 @@ server <- function(input, output) {
       frame <- forecast(fit, n_days)
       frames[,i] <- frame$mean
     }
-    sz <- length(counties) + 1
+    sz <- as.integer( length(counties) * (filter_c / 100) )
+    if (sz == 0 ) {
+      ts.plot()
+    }
     
-    ts.plot(frames, col=2:sz)      # needs a legend
-    legend('topright', legend=counties, lty=1, col=2:sz)
+    # save frames as a session variable to access when making the table below
+    session$userData$fin_frames <- frames
+    ##
+    
+    # Reorder these values from high to low in case we need to filter down according to sz
+    frames <- t(frames)
+    counties <- counties[order(rowMeans(frames), decreasing = T)]
+    frames <- frames[order(rowMeans(frames), decreasing = T), ]
+    frames <- t(frames)
+    ####
+    
+    
+    
+    ts.plot(frames[,1:sz], col=1:sz,
+            gpars=list(xlab="Days", ylab= col, lty=c(1:3)))      # needs a legend
+    legend('topright', legend=counties[1:sz], lty=1, col=1:sz)
     
   }, height = 1000)
+  
+  
+  
+  output$current_counties  <- renderTable({
+    
+    col = input$select1 # this is here just so the table will update to new data
+    n_days = input$bins
+    state = input$select2
+    filter_c = input$filter_c
+    
+    covid_TS_counties_long.pred <- read_csv("data/csv/time_series/covid_TS_counties_long.pred.csv")
+    
+    data <- covid_TS_counties_long.pred
+    data <- data[which(data$State == state),]
+    dates = (unique(data$date))
+    counties <- unique(data$County)
+    sz <- as.integer( length(counties) * (filter_c / 100) )
+    
+    future_dates <- seq(as.Date(dates[length(dates)-1]),by='days',length=n_days)
+    frames <- t(session$userData$fin_frames)
+    counties <- counties[order(rowMeans(frames), decreasing = T)]
+    frames <- frames[order(rowMeans(frames), decreasing = T), ]
+    
+    ret <- as.data.table(frames[1:sz,])
+    colnames(ret) <- as.character(future_dates)
+    ret <- ret %>%
+      mutate(County = counties[1:sz]) %>%
+      dplyr::select(County, everything())
+    
+    
+    ret
+    
+  })
+  
+  
+  
+  # STATE PLOT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   output$state_plot <- renderPlot({
     
     library(forecast)
     library(dplyr)
-    library("plyr")
     library(tidyverse)
     
     n_days = input$bins
@@ -200,6 +269,8 @@ server <- function(input, output) {
       frame <- forecast(fit, n_days)
       frames[,i] <- frame$mean
     }
+    
+    
     sz <- length(states) + 1
     
     ts.plot(frames, col=2:sz)      # needs a legend
